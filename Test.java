8f6c7221491c8372c904b90f09ee728a7dbd6d2b
51600100 <43369623+rongcon973@users.noreply.github.com>
2018-12-21 00:08:23 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,32 +1,27 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+/**
+ * Tác giả: Phạm Anh Tuấn
+ * 
+ * 
+ */
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        
         String s1,s2;
 
-        BufferedReader reader;
-
+        BufferedReader reader; // Lớp đọc giá trị nhập vào
         reader = new BufferedReader(new InputStreamReader(System.in));
 
-
-        try { 
-            System.out.print("s1 = ");
-            s1 = reader.readLine();
-            System.out.print("s2 = ");
-            s2 = reader.readLine();
-            
-            ArrayList<String> result =  new MyBigNumber(s1,s2).sum();
-            for(String x : result){
-                System.out.println(x);
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+        System.out.println("\t\t\t~ BAI TOAN CONG 2 SO ~");
+        System.out.print("Nhap vao so A = ");
+        s1 = reader.readLine();
+        System.out.print("Nhap vao so B = ");
+        s2 = reader.readLine();
+        System.out.println("\t\t\t   ~ Bai giai ~");
+        String[] a = {s1,s2};
+        MyBigNumber.main(a);
     }
 }
