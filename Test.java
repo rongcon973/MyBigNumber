8d16c8eec8d583ed43480223f701afb0bc19b9fb
51600100 <43369623+rongcon973@users.noreply.github.com>
2018-12-23 01:32:06 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,27 +1,125 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-/**
- * Tác giả: Phạm Anh Tuấn
- * 
- * 
- */
+import static org.junit.Assert.*;
 
 public class Test {
-    public static void main(String[] args) throws Exception{
-        
-        String s1,s2;
 
-        BufferedReader reader; // Lớp đọc giá trị nhập vào
-        reader = new BufferedReader(new InputStreamReader(System.in));
+    MyBigNumber myBigNumber = new MyBigNumber();
 
-        System.out.println("\t\t\t~ BAI TOAN CONG 2 SO ~");
-        System.out.print("Nhap vao so A = ");
-        s1 = reader.readLine();
-        System.out.print("Nhap vao so B = ");
-        s2 = reader.readLine();
-        System.out.println("\t\t\t   ~ Bai giai ~");
-        String[] a = {s1,s2};
-        MyBigNumber.main(a);
+    @org.junit.Test
+    public void sum1() {
+        try {
+            String s = myBigNumber.sum("123", "456");
+            assertEquals("579", s);
+        } catch (NullPointerException e) {
+        }
     }
+
+    @org.junit.Test
+    public void sum2() {
+        try {
+            String s = myBigNumber.sum("1235", "856");
+            assertEquals("2091", s);
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @org.junit.Test
+    public void sum3() {
+        try {
+            String s = myBigNumber.sum("100", "900");
+            assertEquals("1000", s);
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @org.junit.Test
+    public void sum4() {
+        try {
+            String s = myBigNumber.sum("999", "111");
+            assertEquals("1110", s);
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @org.junit.Test
+    public void sum5() {
+        try {
+            String s = myBigNumber.sum("999", "1111");
+            assertEquals("2110", s);
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @org.junit.Test
+    public void sum6() {
+        try {
+            String s = myBigNumber.sum("1234567890", "4");
+            assertEquals("1234567894", s);
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @org.junit.Test(expected = NumberFormatException.class)
+    public void sum7() {
+        try {
+            String s = myBigNumber.sum("abs", "4");
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @org.junit.Test(expected = NumberFormatException.class)
+    public void sum8() {
+        try {
+            String s = myBigNumber.sum("-3", "4");
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @org.junit.Test(expected = NumberFormatException.class)
+    public void sum9() {
+        try {
+            String s = myBigNumber.sum("3", "4asd");
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @org.junit.Test(expected = NumberFormatException.class)
+    public void sum10() {
+        try {
+            String s = myBigNumber.sum("3", "-7");
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @org.junit.Test(expected = NumberFormatException.class)
+    public void sum11() {
+        try {
+            String s = myBigNumber.sum("-13", "-7");
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @org.junit.Test(expected = NumberFormatException.class)
+    public void sum12() {
+        try {
+            String s = myBigNumber.sum("3", "-7asdg");
+        } catch (NullPointerException e) {
+        }
+    }
+
+    @org.junit.Test(expected = NumberFormatException.class)
+    public void sum13() {
+        MyBigMain test = new MyBigMain();
+        MyBigNumber myBigNumber = new MyBigNumber(test);
+        String s = myBigNumber.sum("3", "-7asdg");
+    }
+
+    @org.junit.Test
+    public void sum14() {
+        try {
+            String s = myBigNumber.sum("111", "999");
+            assertEquals("1110", s);
+        } catch (NullPointerException e) {
+        }
+    }
+
 }
